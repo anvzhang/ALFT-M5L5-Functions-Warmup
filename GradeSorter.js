b6892948a2c5443d06b1be823b8545f4d42a3ae6
MichaelOppong <OppongM@gmail.com>
2024-05-03 01:41:17 +0000

    Initial commit

---
+++ GradeSorter.js
@@ -0,0 +1,8 @@
+/* Sort the Grades!
+Use loops to sort the array of grades into passing and failing grades.
+
+All passing grades (65 or higher) should log to the console in one list, and the rest should be logged in a failing list.
+
+*/
+
+let grades = [ 99,100,85,62,35,20,76,54,34,78,92,83,55,45,27,52,87,94,10,0,90,50,78];
---
+++ README.md
@@ -0,0 +1,6 @@
+## ALFT: M5L5 - Functions '24
+
+Work through the drills in the different JS files. The first is the easiest and the last is the hardest.
+1. Billboard Hot 100
+2. Tallest Mountains
+3. Grade Sorter
